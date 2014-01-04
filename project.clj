f27dff763af5b64a23c005e56dd8fed9a0dbdc51
Morgawr <morgawr@gmail.com>
2014-01-04 18:15:23 +0100

    removed old stuff

--- project.clj
+++ project.clj
@@ -8,9 +8,6 @@
                  [org.lwjgl.lwjgl/lwjgl "2.9.0"]
                  [org.lwjgl.lwjgl/lwjgl_util "2.9.0"]
                  [slick-util "1.0.0"]]
-                 ;[slick-util "0.1.0"]]
-  ;:repositories { "local" ~(str (.toURI (java.io.File. "local_maven")))}
-  ;:local-repo "local_maven"
   :jvm-opts [~(str "-Djava.library.path=native/:" (System/getProperty "java.library.path"))]
   :plugins [[lein-marginalia "0.7.1"]]
   :repl-options { :nrepl-middleware [lighttable.nrepl.handler/lighttable-ops] } )
